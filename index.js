8915fe60081c6df5d72d68c61b83d721f0a1d8aa
Gwakman <gwakeur@gmail.com>
2020-10-29 15:40:42 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '\\index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
+
+
+/*
+Homework
+Here are some ideas to improve the application:
+
+- Broadcast a message to connected users when someone connects or disconnects.
+- Add support for nicknames.
+- Don’t send the same message to the user that sent it. Instead, append the message directly as soon as he/she presses enter.
+- Add “{user} is typing” functionality.
+- Show who’s online.
+- Add private messaging.
+- Share your improvements!
+*/
